08c9e2a9aefbdf82b9c4eb63efcf75aa6b40b836
Akonaho <64597136+Akonaho@users.noreply.github.com>
2023-05-23 17:38:26 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
   public static void main(String[] args){
     // Prints "Hello, Grant" on the terminal
-    System.out.println("Hello, Gratnt");
+    System.out.println("Hello, Grant");
   }
 }
